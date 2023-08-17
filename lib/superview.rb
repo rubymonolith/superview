7b6a11ee0caaa281e1bc6c00529cccafd0d90576
Brad Gessler <bradgessler@gmail.com>
2023-08-17 15:53:55 -0700

    Initial commit

---
+++ lib/superview.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "superview/version"
+
+module Superview
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/superview/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Superview
+  VERSION = "0.1.0"
+end
