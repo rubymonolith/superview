065275fb2bcfdd0a0d8838a519512afd2b3e78e1
Brad Gessler <bradgessler@gmail.com>
2024-12-20 21:46:39 -0800

    Change `phlex_` methods to `component_` methods. Alias `phlex` to the `component` method so it doesn't break.

--- lib/superview/actions.rb
+++ lib/superview/actions.rb
@@ -37,7 +37,7 @@ module Superview
       # Finds a class on the controller with the same name as the action. For example,
       # `def index` would find the `Index` constant on the controller class to render
       # for the action `index`.
-      def phlex_action_class(action:)
+      def component_action_class(action:)
         action_class = action.to_s.camelcase
         const_get action_class if const_defined? action_class
       end
@@ -49,7 +49,7 @@ module Superview
     # on Phlex. For example, if a controller defines @users and a Phlex class has
     # `attr_writer :users`, `attr_accessor :user`, or `def users=`, it will be automatically
     # set by this method.
-    def assign_phlex_accessors(view)
+    def assign_component_accessors(view)
       view.tap do |view|
         view_assigns.each do |variable, value|
           attr_writer_name = "#{variable}="
@@ -59,44 +59,45 @@ module Superview
     end
 
     # Initializers a Phlex view based on the action name and assigns accessors
-    def phlex_action(action)
-      phlex_view self.class.phlex_action_class(action: action)
+    def component_action(action)
+      component_view self.class.component_action_class(action: action)
     end
 
-    # Initializes a phlex view class and assigns accessors.
-    def phlex_view(view_class)
-      assign_phlex_accessors view_class.new
+    # Initializes a component view class and assigns accessors.
+    def component_view(view_class)
+      assign_component_accessors view_class.new
     end
 
     # Phlex action for the current action.
-    def phlex(target = action_name)
-      case target
-      when Phlex::HTML
-        assign_phlex_accessors target
-      when Symbol, String
-        phlex_action target
-      when Class
-        phlex_view target
+    def component(target = action_name)
+      if target.is_a? Class
+        component_view target
+      elsif target.respond_to? :render_in
+        assign_component_accessors target
+      else
+        component_action target
       end
     end
 
+    alias :phlex :component
+
     # Checks if a Phlex class name is present for a controller action name
-    def phlex_action_exists?(action)
-      self.class.phlex_action_class(action: action).present?
+    def component_action_exists?(action)
+      self.class.component_action_class(action: action).present?
     end
 
     # This is a built-in Rails method resolves the method to call for an action.
     # If it resolves a Phlex class in the controller, it will render that. If it's
     # not found it continues with Rails method of resolving action names.
     def method_for_action(action_name)
-      super || if phlex_action_exists? action_name
-                 "default_phlex_render"
+      super || if component_action_exists? action_name
+                 "default_component_render"
                end
     end
 
     # Renders a Phlex view for the given action, if it's present.
-    def default_phlex_render
-      render phlex
+    def default_component_render
+      render component
     end
   end
 end
